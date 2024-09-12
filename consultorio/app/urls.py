25b7211371c3f185d8dbc0d064d28947109c4bd6
Roberto Franco <roberto.yuri.franco@gmail.com>
2024-09-11 21:06:46 -0300

    html, css, bootstrap create: paciente medico consultorio
    read:
    paciente
    medico
    
    url, views

--- consultorio/app/urls.py
+++ consultorio/app/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, second
+from .views import home, second, createpac, createmed, createcons, readpac, readmed
 
 urlpatterns = [
     path('', home, name='home'),
+    path('createpac/', createpac, name='createpac'),
+    path('createmed/', createmed, name='createmed'),
+    path('createcons/', createcons, name='createcons'),
+    path('readpac/', readpac, name='readpac'),
+    path('readmed/', readmed, name='readmed'),
     path('second/', second, name='second'),
 ]
--- consultorio/app/views.py
+++ consultorio/app/views.py
@@ -7,3 +7,18 @@ def home(request):
 
 def second(request):
     return render(request, 'second.html')
+
+def createpac(request):
+    return render(request, 'createpac.html')
+
+def createmed(request):
+    return render(request, 'createmed.html')
+
+def createcons(request):
+    return render(request, 'createcons.html')
+
+def readpac(request):
+    return render(request, 'readpac.html')
+
+def readmed(request):
+    return render(request, 'readmed.html')
